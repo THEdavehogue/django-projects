960484098be9aec617ca682ef224b5d6c04fa3d0
Dave and Rachel Hogue <daveandrachelhogue@Daves-iMac.local>
2017-01-17 14:40:59 -0700

    add contact page to personal site

--- mynewsite/personal/views.py
+++ mynewsite/personal/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content': ['If you would like to contact me, please email me', 'thedavehogue@gmail.com']})
+    
